4b7fd3cba321b1dacd3c55c908981421801495d2
Thomas <173218@ids.upchiapas.edu.mx>
2021-07-30 15:44:41 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',component:LoginComponent
+  },
+  {
+    path:'dashboard',loadChildren:()=>import ('./components/dashboard/dashboard.module').then(x => x.DashboardModule)
+  },
+  {
+    path:'**',redirectTo:'',pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AdminComponent } from './components/dashboard/admin/admin.component';
-import { BottombarComponent } from './components/dashboard/bottombar/bottombar.component';
-import { ClasschooserComponent } from './components/dashboard/classchooser/classchooser.component';
-import { DetailDialogComponent } from './components/dashboard/detail-dialog/detail-dialog.component';
-import { EditUserComponent } from './components/dashboard/edit-user/edit-user.component';
-import { ListDialogComponent } from './components/dashboard/list-dialog/list-dialog.component';
-import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
-import { RegisterComponent } from './components/dashboard/register/register.component';
-import { TakeclassdialogComponent } from './components/dashboard/takeclassdialog/takeclassdialog.component';
+import { SharedModule } from './components/shared/shared.module';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    AdminComponent,
-    BottombarComponent,
-    ClasschooserComponent,
-    DetailDialogComponent,
-    EditUserComponent,
-    ListDialogComponent,
-    NavbarComponent,
-    RegisterComponent,
-    TakeclassdialogComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
