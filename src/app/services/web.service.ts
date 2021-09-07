b0562a8a757d3896616f8d7985bda455fe8e570b
Thomas <173218@ids.upchiapas.edu.mx>
2021-09-06 19:23:44 -0500

    renew visual components

--- src/app/services/web.service.ts
+++ src/app/services/web.service.ts
@@ -9,8 +9,8 @@ import { Language, User } from '../interfaces/interfaces';
 
 
 export class WebService {
-  link = 'https://linguazone2.herokuapp.com/';
-  //link = 'http://localhost:8000/'
+  //link = 'https://linguazone2.herokuapp.com/';
+  link = 'http://localhost:8000/'
   books = '';
   classLink='';
   list = '';
